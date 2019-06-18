340605b9f57bce4d1631b7f618a285c8a4917eb0
Joel Bebo <joelbebo@gmail.com>
2019-06-18 18:29:01 -0400

    add the add page

--- todolist/urls.py
+++ todolist/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+# from django.contrib import admin
+# from django.urls import path, include
+#
+#
+# urlpatterns = [
+#     path('', include("todos.urls")),
+#     path('admin/', admin.site.urls),
+# ]
 
+
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
-    path('', include("todos.urls")),
-    path('admin/', admin.site.urls),
+    url(r'^', include("todos.urls")),
+    url(r'^admin/', admin.site.urls),
 ]
--- todos/urls.py
+++ todos/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path
+# from django.urls import path
+#
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('todos/', views.index, name='index'),
+#     # path('details/(?P<id>\w{0,50})/', views.details)
+#     path('details/', views.details)
+# ]
+
+
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('todos/', views.index, name='index'),
-    path('details/(?P<id>\w{0,50})/', views.details)
-    #path('details/', views.details)
-
+    url(r'^$', views.index, name='index'),
+    url(r'^details/(?P<id>\w{0,50})/$', views.details),
+    url(r'^add/(?P<id>\w{0,50})/$', views.add)
 ]
