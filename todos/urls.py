72767bd6e10b834e9d197dc9bf3a146b4bfb5b7d
Joel Bebo <joelbebo@gmail.com>
2019-06-18 19:36:39 -0400

    frontend link to database

--- todos/urls.py
+++ todos/urls.py
@@ -17,5 +17,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details),
-    url(r'^add/(?P<id>\w{0,50})/$', views.add)
+    url(r'^add', views.add, name='add')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -13,6 +13,7 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+
 def details(request, id):
     todo = Todo.objects.get(id=id)
 
@@ -20,3 +21,17 @@ def details(request, id):
         'todo': 'todo'
     }
     return render(request, 'details.html', context)
+
+
+def add(request):
+    if(request.method == "POST"):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/todos')
+
+    else:
+        return render(request, 'add.html')
